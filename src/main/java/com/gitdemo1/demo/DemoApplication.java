d28c32c6aa8ba1c2c3eca95b36e3cde827f14eed
YuYUSvn <57694743+YuYUSvn@users.noreply.github.com>
2019-11-13 10:56:41 +0800

    Add files via upload

---
+++ src/main/java/com/gitdemo1/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.gitdemo1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
